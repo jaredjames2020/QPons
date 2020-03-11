66f281b474b9b2cee2acf10f19b750aa70745c2a
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-11 11:05:36 -0400

    added routes for views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :companies, only: [:new, :create, :index]
+  resources :offers, only: [:new, :create, :index]
+  resources :paths, only: [:new, :create, :index]
+
 end
