c255a0c63b7e886042119a1e264a97c8484dfac5
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-11 15:19:23 -0400

    reordered model has_many relationships

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,10 @@
 class Company < ApplicationRecord
-  has_many :travels, through: :offers
   has_many :offers
+  has_many :travels, through: :offers
   validates_presence_of :name, :location
 
   def company_offers_path
-    binding.pry
+
   end
 
 
--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,4 +1,5 @@
 class Travel < ApplicationRecord
   has_many :users
+  has_many :offers
   has_many :companies, through: :offers
 end
