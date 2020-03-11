6b6e48c5f9aba8db1aa3ad1aa2b5f6c4ee2e451f
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-11 14:41:31 -0400

    add actions to travels controller - new, index, create, show, private travel_params

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,12 +1,31 @@
 class TravelsController < ApplicationController
 
   def new
+    @travel = Travel.new
   end
 
   def index
+    @travels = Travel.all
   end
 
   def create
+    @travel = Travel.new(travel_params)
+      if @travel.save
+        redirect_to travels_path
+      else
+        render :new
+      end
+  end
+
+  def show
+    @travel = Travel.find(params[:id])
+  end
+
+
+  private
+
+  def travel_params
+    params.require(:travel).permit(:name)
   end
 
 end
