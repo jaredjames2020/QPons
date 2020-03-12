7ff38138075e6b12438f89d074f47e63d879634f
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 18:59:12 -0400

    fixed typo to username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :offers
   has_many :companies
   has_many :travels, through: :offers
-  validates_presence_of :usersname, :password, :password_confirmation
+  validates_presence_of :username, :password, :password_confirmation
   validates_uniqueness_of :username
   has_secure_password
 end
