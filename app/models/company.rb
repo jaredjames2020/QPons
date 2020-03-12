1c77b9446b92315d9101371a851db39ba54bcae3
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 15:31:26 -0400

    added scope methods to filter by company category

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,13 @@ class Company < ApplicationRecord
   has_many :offers
   has_many :travels, through: :offers
   validates_presence_of :name, :location
+  scope :food, -> {where(category: 'Food') }
+  scope :technology, -> {where(category: 'Technology') }
+  scope :clothing, -> {where(category: 'Clothing') }
+  scope :auto, -> {where(category: 'auto') }
+  scope :footwear, -> {where(category: 'Footwear') }
+  scope :drugstore, -> {where(category: 'Drugstore') }
+
 
   def company_offers_path
 
