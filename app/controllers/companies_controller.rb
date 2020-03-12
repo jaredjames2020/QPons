91e4e5d27c9808f4798a8e8d32849ad300957122
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 14:51:44 -0400

    companies controller updates

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,17 +1,19 @@
 class CompaniesController < ApplicationController
 
   def new
+    get_current_travel
     @company = Company.new
   end
 
   def index
+    get_current_travel
     @companies = Company.all
   end
 
   def create
     @company = Company.new(company_params)
       if @company.save
-        redirect_to companies_path
+        redirect_to travel_path(params[:travel_id])
       else
         render :new
       end
@@ -25,7 +27,11 @@ class CompaniesController < ApplicationController
   private
 
   def company_params
-    params.require(:company).permit(:name, :location, :category)
+    params.require(:company).permit(:company_id, :name, :location, :category)
+  end
+
+  def get_current_travel
+    @travel ||= Travel.find_by(id: params[:travel_id])
   end
 
 end
