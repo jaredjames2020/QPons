78b36a3f13b3c2e95cfe819e1522233812a13d8a
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 14:43:07 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :travels, only: [:new, :create, :index, :show] do
+    resources :companies, only: [:new, :create, :index, :show]
+    resources :offers, only: [:new, :index, :create]
+  end
+  resources :offers, only: [:create]
   resources :companies, only: [:new, :create, :index, :show]
-  resources :offers, only: [:new, :create, :index]
-  resources :travels, only: [:new, :create, :index, :show]
 
 end
