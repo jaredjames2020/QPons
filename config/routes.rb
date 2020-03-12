01087a9834f86ae7200fa47f2f23998db191dd1c
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 16:46:07 -0400

    added custom routes for scope method views

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :travels, only: [:new, :create, :index, :show] do
-    resources :companies, only: [:new, :create, :index, :show]
+    resources :companies, only: [:new, :create, :index]
     resources :offers, only: [:new, :index, :create]
   end
   resources :offers, only: [:create]
-  resources :companies, only: [:new, :create, :index, :show]
+  resources :companies, only: [:new, :create, :index]
+  get '/companies/food', to: 'companies#food'
+  get '/companies/technology', to: 'companies#technology'
+  get '/companies/clothing', to: 'companies#clothing'
+  get '/companies/auto', to: 'companies#auto'
+  get '/companies/footwear', to: 'companies#footwear'
+  get '/companies/drugstore', to: 'companies#drugstore'
+
 
 end
