6d21677415a1b54203c2b2b0bb781dad08c94983
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 18:58:55 -0400

    add index and create methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
-  def index
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to new_company_path
+    else
+      render :new
+    end
   end
 
-  def creates
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
 end
