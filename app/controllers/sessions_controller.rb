774223015854e70dfd55842deaf1b94197576874
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-12 19:39:55 -0400

    sessions controller - new, create, destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to travels_path
+    else
+      @error = "Please try again"
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to travels_path
+  end
+
 end
