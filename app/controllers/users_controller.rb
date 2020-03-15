4ecd7a31a23a0df76fa60faa5bf735ce3d908bf6
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-15 13:28:40 -0400

    edits to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      # session[:user_id] = @user.id
+      log_in(@user)
       redirect_to travels_path
     else
       render :new
