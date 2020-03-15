35c2ef00532392b16ab73aeef456aaa471c5acdb
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-15 13:47:27 -0400

    add relationships to user model, validations, and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
+   has_many :offers
+   has_many :companies
+   has_many :travels, through: :offers
+   validates_presence_of :username, :password, :password_confirmation
+   validates_uniqueness_of :username
+   has_secure_password
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
