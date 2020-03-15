392fc25865fb6d55b4bab48157874d79797c6007
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-15 13:25:22 -0400

    updated routes for devise_scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'application#welcome'
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
+  devise_scope :users do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :travels, only: [:new, :create, :index, :show] do
