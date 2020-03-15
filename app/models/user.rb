590639a2b3f1bf36376c6ca520e01ed3a9cfd88b
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-15 13:28:05 -0400

    updated user model for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
-  has_many :offers
-  has_many :companies
-  has_many :travels, through: :offers
-  validates_presence_of :username, :password, :password_confirmation
-  validates_uniqueness_of :username
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.username = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
