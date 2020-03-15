220ab23e3c115983f12045841902f7de22055e8a
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-15 13:30:38 -0400

    application controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  def welcome
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    redirect_to travels_path unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
 end
