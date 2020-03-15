1e46f05ca876cf3881437b82ee7036734203343f
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-15 13:23:38 -0400

    updated scheme file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_222815) do
+ActiveRecord::Schema.define(version: 2020_03_15_164737) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -37,11 +37,16 @@ ActiveRecord::Schema.define(version: 2020_03_12_222815) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "username"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
