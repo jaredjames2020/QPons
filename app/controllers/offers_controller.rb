099ec4cb6c16c1253a40334847e9970491b44ef5
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-16 19:44:19 -0400

    refactoring controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,20 +1,33 @@
 class OffersController < ApplicationController
+  before_action :require_login, only: [:new, :create]
 
   def new
-    @offer = Offer.new
+    # @offer = Offer.new
+    # @companies = current_user.companies
+    # @company = current_user.companies.new
+    current_user.companies.build
+    # @company.offers.build
+    @user = current_user
+    @companies = current_user.companies
+    @company = current_user.companies.new
+    @travel = Travel.all
+    # @all_companies = Company.distinct.pluck(:category)
+    @all_companies = Company.all
     find_travel
   end
 
   def index
-    @offers = Offer.all
+      @offers = Offer.all
+    # @offers = current_user.companies
   end
 
   def create
     @offer = Offer.new(offer_params)
-    @offer.company_attributes={ name: params[:offer][:companies][:name],
-                                location: params[:offer][:companies][:location],
-                                category: params[:offer][:companies][:category]
-                              }
+    # @offer.company_attributes={ name: params[:offer][:companies][:name],
+    #                             location: params[:offer][:companies][:location],
+    #                             category: params[:offer][:companies][:category]
+    #                           }
+    #                           binding.pry
       if @offer.save
         redirect_to travel_path(@offer.travel.id)
       else
@@ -32,7 +45,7 @@ class OffersController < ApplicationController
   private
 
   def offer_params
-    params.require(:offer).permit(:description, :company_id, :travel_id, company: [:name, :location, :category])
+    params.require(:offer).permit(:description, :travel_id, company_attributes: [:name, :location, :category])
   end
 
   def find_travel
