1ca4a1974f1ca7c652e289fa511e154b89a78d15
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-16 19:44:11 -0400

    refactoring controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,18 @@
 class CompaniesController < ApplicationController
-  #before_action :require_login
+  before_action :require_login, only: [:new, :create]
+  before_action :get_current_companies, only: [:new, :create]
 
   def new
     get_current_travel
-    @company = Company.new
+    # @company = Company.new
+    current_user.companies.build
+    # @company.offers.build
+    @user = current_user
+    # @companies = current_user.companies
+    @company = current_user.companies.new
+    @travel = Travel.all
+    # @all_companies = Company.distinct.pluck(:category)
+    @all_companies = Company.all
   end
 
   def index
@@ -11,12 +20,18 @@ class CompaniesController < ApplicationController
     # @companies = Company.all
     # @food = Company.food
     # @user = User.find_by(id: session[:user_id])
+    @user = current_user
     @companies = current_user.companies
+    @company = current_user.companies.new
+    @travel = Travel.all
+    # @all_companies = Company.distinct.pluck(:category)
+    @all_companies = Company.all
   end
 
   def create
-    # @company = Company.new(company_params)
-    @company = current_user.companies.build(company_params)
+    # if user_signed_in?
+    @company = current_user.companies.new(company_params)
+    # @company.offers.new.description = (params[:company][:offers_attributes]["0"][:description])
       if @company.save
         # redirect_to travel_path(params[:travel_id])
         redirect_to companies_path
@@ -56,11 +71,15 @@ class CompaniesController < ApplicationController
   private
 
   def company_params
-    params.require(:company).permit(:company_id, :name, :location, :category)
+    params.require(:company).permit(:company_id, :name, :location, :category, offers_attributes: [:description, :travel_id])
   end
 
   def get_current_travel
     @travel ||= Travel.find_by(id: params[:travel_id])
+  end
+
+  def get_current_companies
+    @company ||=Company.find_by(id: params[:company_id])
   end
 
 end
