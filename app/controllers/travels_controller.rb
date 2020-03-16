eee143a45213d0cb043099074db5018b1b9f54ac
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-16 19:44:36 -0400

    adding before action to travels controller

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,11 +1,14 @@
 class TravelsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+  before_action :get_travel_route, only: [:new, :index]
+
 
   def new
     @travel = Travel.new
   end
 
   def index
-    @travels = Travel.all
+      @travels = Travel.all
   end
 
   def create
@@ -18,7 +21,35 @@ class TravelsController < ApplicationController
   end
 
   def show
-    @travel = Travel.find(params[:id])
+    get_travel_route ? (get_travel_route) : (redirect_to root_path)
+  end
+
+  def auto
+    @auto = Company.auto
+  end
+
+  def clothing
+    @clothing = Company.clothing
+  end
+
+  def drugstore
+    @drugstore = Company.drugstore
+  end
+
+  def food
+    @food = Company.food
+  end
+
+  def footwear
+    @footwear = Company.footwear
+  end
+
+  def technology
+    @technology = Company.technology
+  end
+
+  def all
+    @offers = Offers.all
   end
 
 
@@ -26,6 +57,10 @@ class TravelsController < ApplicationController
 
   def travel_params
     params.require(:travel).permit(:name)
+  end
+
+  def get_travel_route
+    @travel ||= Travel.find_by(id: params[:id])
   end
 
 end
