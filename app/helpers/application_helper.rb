4faf68a0edb33872a5e1e85c03bb4e8483ef6966
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-16 19:45:26 -0400

    helpers for application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def admin
+    current_user.username == "admin"
+  end
+
+  def admin_email
+    current_user.email == "ku7%4ffth%43@oPmn"
+  end
+
+  def current_user_offers
+    Company.find_by(user_id: current_user.id).offers
+  end
+  
 end
