72822e398b293792ec281b7e85c76e20d9c0bf9f
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-24 10:55:39 -0400

    edit to devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'application#welcome'
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
-  devise_scope :users do
+  devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
