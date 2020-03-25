2e6725ee2957ea78acb552687bfb4e1f80c22abf
jaredjames2020 <jaredjames2020@gmail.com>
2020-03-24 22:53:40 -0400

    cleaned code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  def welcome
-  end
-
-  # def current_user
-  #   User.find_by(id: session[:user_id])
-  # end
-
   def require_login
     redirect_to root_path unless logged_in?
   end
